6a76083362ac6dece74910a0dd5a5df62a230692
Mariele Evangelista <164822586+mwriele@users.noreply.github.com>
2024-05-27 20:09:10 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+import classes.ProdutoDAO;
+
+public class Main {
+    public static void main(String[] args) {
+        ProdutoDAO produtoDAO = new ProdutoDAO();
+        produtoDAO.menuOperacoes();
+    }
+}
